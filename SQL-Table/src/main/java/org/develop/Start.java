70c4a9f4c3d7724b31b27e7fd7c1ebf2799e2e13
Marco De Fenzo <82841719+Kram037@users.noreply.github.com>
2022-12-13 12:21:47 +0100

    Add files via upload

---
+++ SQL-Table/src/main/java/org/develop/Start.java
@@ -0,0 +1,49 @@
+package org.develop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+
+        try{
+            String url = "jdbc:mysql://localhost:3306/newdb";
+            String user = "root";
+            String password = "PinoDaniele03";
+
+            con = DriverManager.getConnection(url, user, password);
+
+            Statement s = con.createStatement();
+
+            String sql = "CREATE TABLE student " +
+                         "(student_id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                         " last_name VARCHAR(30), " +
+                         " first_name VARCHAR(30))";
+
+            s.executeUpdate(sql);
+
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('De Fenzo', 'Marco');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Vassarotti', 'Vittoria');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Di Leo', 'Luca');");
+            s.execute("INSERT INTO newdb.student (last_name, first_name) VALUES('Mustata', 'Alina Elena');");
+
+
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            try{
+                if(con != null)
+                    con.close();
+            }catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
+
